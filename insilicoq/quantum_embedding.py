c792331caa243de95a1290fb264b065249633be8
Farhad Abdi <qai.abdi@gmail.com>
2022-07-02 06:39:18 +0430

    Updated Data Encoding

--- insilicoq/quantum_embedding.py
+++ insilicoq/quantum_embedding.py
@@ -10,30 +10,22 @@ from qiskit.circuit.library import ZFeatureMap
  
 
 def angle_encoding(num_qubit, data):
-'''
- Args:
- num_qubit : number of feature space dimesnion
- data : 1D array real data to be encoded
-
- Return:
- Quantum Data Circuit Object
- 
-'''
- data  = normalize(data)
- feature_map = ZFeatureMap(feature_dimension= num_qubit, reps = 1 )
- data_circ = featuremap.assign_parameters(data)
-
- return data_circ
- 
-
-def basis_encodeing(num_qubit, int_data):
+    
 
 
- 
- return quantum_circ
- 
- 
-def amplitue_encoding(num_qubit, data):
+  '''
+  Args:
+  num_qubit : number of feature space dimesnion
+  data : 1D array real data to be encoded
 
+  Return:
+  Quantum Data Circuit Object 
+  
+  '''
+  
+  data  = normalize(data)
+  feature_map = ZFeatureMap(feature_dimension= num_qubit, reps = 1 )
+  data_circ = feature_map.assign_parameters(data)
+
+  return data_circ
  
- return quantum_circ
