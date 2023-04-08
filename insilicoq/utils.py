301f7ea41e55252f3745a6845d7f26cccd2dff35
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-08 16:28:58 +0330

    Create utils.py

---
+++ insilicoq/utils.py
@@ -0,0 +1,100 @@
+from rdkit import Chem, RDLogger
+from rdkit.Chem.Draw import IPythonConsole, MolsToGridImage
+import numpy as np
+
+#some helper function for rdkit
+
+atom_mapping = {
+    "C": 0,
+    0: "C",
+    "N": 1,
+    1: "N",
+    "O": 2,
+    2: "O",
+    "F": 3,
+    3: "F",
+}
+
+bond_mapping = {
+    "SINGLE": 0,
+    0: Chem.BondType.SINGLE,
+    "DOUBLE": 1,
+    1: Chem.BondType.DOUBLE,
+    "TRIPLE": 2,
+    2: Chem.BondType.TRIPLE,
+    "AROMATIC": 3,
+    3: Chem.BondType.AROMATIC,
+}
+
+NUM_ATOMS = 9  # Maximum number of atoms
+ATOM_DIM = 4 + 1  # Number of atom types
+BOND_DIM = 4 + 1  # Number of bond types
+
+
+def smiles_to_graph(smiles):
+    # Converts SMILES to molecule object
+    molecule = Chem.MolFromSmiles(smiles)
+
+    # Initialize adjacency and feature tensor
+    adjacency = np.zeros((BOND_DIM, NUM_ATOMS, NUM_ATOMS), "float32")
+    features = np.zeros((NUM_ATOMS, ATOM_DIM), "float32")
+
+    # loop over each atom in molecule
+    for atom in molecule.GetAtoms():
+        i = atom.GetIdx()
+        atom_type = atom_mapping[atom.GetSymbol()]
+        features[i] = np.eye(ATOM_DIM)[atom_type]
+        # loop over one-hop neighbors
+        for neighbor in atom.GetNeighbors():
+            j = neighbor.GetIdx()
+            bond = molecule.GetBondBetweenAtoms(i, j)
+            bond_type_idx = bond_mapping[bond.GetBondType().name]
+            adjacency[bond_type_idx, [i, j], [j, i]] = 1
+
+    # Where no bond, add 1 to last channel (indicating "non-bond")
+    # Notice: channels-first
+    adjacency[-1, np.sum(adjacency, axis=0) == 0] = 1
+
+    # Where no atom, add 1 to last column (indicating "non-atom")
+    features[np.where(np.sum(features, axis=1) == 0)[0], -1] = 1
+
+    return adjacency, features
+
+
+def graph_to_molecule(graph):
+    # Unpack graph
+    adjacency, features = graph
+
+    # RWMol is a molecule object intended to be edited
+    molecule = Chem.RWMol()
+
+    # Remove "no atoms" & atoms with no bonds
+    keep_idx = np.where(
+        (np.argmax(features, axis=1) != ATOM_DIM - 1)
+        & (np.sum(adjacency[:-1], axis=(0, 1)) != 0)
+    )[0]
+    features = features[keep_idx]
+    adjacency = adjacency[:, keep_idx, :][:, :, keep_idx]
+
+    # Add atoms to molecule
+    for atom_type_idx in np.argmax(features, axis=1):
+        atom = Chem.Atom(atom_mapping[atom_type_idx])
+        _ = molecule.AddAtom(atom)
+
+    # Add bonds between atoms in molecule; based on the upper triangles
+    # of the [symmetric] adjacency tensor
+    (bonds_ij, atoms_i, atoms_j) = np.where(np.triu(adjacency) == 1)
+    for (bond_ij, atom_i, atom_j) in zip(bonds_ij, atoms_i, atoms_j):
+        if atom_i == atom_j or bond_ij == BOND_DIM - 1:
+            continue
+        bond_type = bond_mapping[bond_ij]
+        molecule.AddBond(int(atom_i), int(atom_j), bond_type)
+
+    # Sanitize the molecule; for more information on sanitization, see
+    # https://www.rdkit.org/docs/RDKit_Book.html#molecular-sanitization
+    flag = Chem.SanitizeMol(molecule, catchErrors=True)
+    # Let's be strict. If sanitization fails, return None
+    if flag != Chem.SanitizeFlags.SANITIZE_NONE:
+        return None
+
+    return molecule
