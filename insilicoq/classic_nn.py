97e22d20d3b1755c4ad5ffac8f2fc3e787f4b856
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-08 17:43:08 +0330

    Create classic_nn.py

---
+++ insilicoq/classic_nn.py
@@ -0,0 +1,23 @@
+# Classical neural networks for using in hybrid setting (GAN, CNN, ..)
+
+# import the necessary packages
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import MaxPool2d
+from torch.nn import ReLU
+from torch.nn import LogSoftmax
+from torch import flatten
+
+
+class Classic_Net(nn.Module):
+  
+  def __init__(self, net_type:str):
+    #net type to be created
+    
+    
+  def forward(self):
+    
