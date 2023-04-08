50c0b185d27ab7d863703f6019c859f4a30e2fee
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-08 17:28:36 +0330

    Create quantum_nn.py

---
+++ insilicoq/quantum_nn.py
@@ -0,0 +1,26 @@
+# The construction of quantum neural networks
+# see https://qiskit.org/documentation/machine-learning/tutorials/01_neural_networks.html#4.1-Output:-sparse-integer-probabilities
+
+from qiskit.circuit import Parameter
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+from qiskit_machine_learning.neural_networks import EstimatorQNN
+from qiskit.circuit import ParameterVector
+from qiskit_machine_learning.neural_networks import SamplerQNN
+
+
+class QNN:
+  
+  def __init__(self):
+    
+    
+    
+  def estimator(self):
+    pass
+  
+  def sampler(self):
+    pass
+  
+  
+    
+    
