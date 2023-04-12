c2159cab79675c517aef708dafe0afa0bc173a19
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-12 19:45:44 +0330

    Update __init__.py

--- insilicoq/__init__.py
+++ insilicoq/__init__.py
@@ -2,6 +2,6 @@ from insilicoq.quantum_kernel import qkern_default, qkern_eq8, qkern_eq9, qkern_
 from insilicoq.quantum_gan import QGAN, QGenerator
 from insilicoq.quantum_embedding import angle_encoding, amplitude_encoding
 from insilicoq.dataset import Dataset
-from insilicoq.classic_nn import ClassicalNet
+from insilicoq.classic_nn import ClassicNet
 from insilicoq.transformers import Embedding
 from insilicoq.utils import smiles_to_graph, graph_to_molecule
