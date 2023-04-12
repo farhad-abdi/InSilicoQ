de364ee88a34437155b188d1847811315467cd44
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-12 19:17:41 +0330

    change to staticmethod

--- insilicoq/dataset.py
+++ insilicoq/dataset.py
@@ -4,18 +4,17 @@ import pandas as pd
 
 # return data frame for diffrent datasets currently 'zinc, 'qm9'
 class Dataset:
+  
+  @staticmethod
+  def get(dataset):
+    qm9_url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/qm9.csv'
+    zinc_url = 'https://raw.githubusercontent.com/aspuru-guzik-group/chemical_vae/master/models/zinc_properties/250k_rndm_zinc_drugs_clean_3.csv'
 
-
-  def __init__(self):
-    self.qm9_url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/qm9.csv'
-    self.zinc_url = 'https://raw.githubusercontent.com/aspuru-guzik-group/chemical_vae/master/models/zinc_properties/250k_rndm_zinc_drugs_clean_3.csv'
-
-  def get(self, dataset):
     if dataset == 'qm9':
-      urllib.request.urlretrieve(self.qm9_url, 'qm9.csv')
+      urllib.request.urlretrieve(qm9_url, 'qm9.csv')
 
     if dataset == 'zinc':
-      urllib.request.urlretrieve(self.zinc_url,'zinc.csv')
+      urllib.request.urlretrieve(zinc_url,'zinc.csv')
 
     df = pd.read_csv(f'{dataset}.csv')
 
