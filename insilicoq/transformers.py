ca10ba2d9bee7a6a67824be095e321ffc8768e64
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-13 17:00:08 +0330

    integrated protot5 half precision

--- insilicoq/transformers.py
+++ insilicoq/transformers.py
@@ -1,44 +1,46 @@
-"""
-Extract embedding from pretrained transformers on protien sequences(ProtoT5, ProtoBert, ESM, ... )
-"""
-
 from transformers import T5EncoderModel, T5Tokenizer
 from transformers import AutoTokenizer, AutoModel, AutoConfig
 
-import esm
+#import esm
 
 import torch
 import torch.nn as nn
 
 
-class Embedding(nn.Module):
+class Embedding:
   
   def __init__(self, model_type:str, input_seq:str):
-    self.model_type = model
+    
+
+    self.model_type = model_type
     self.seq = input_seq
+    self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    print(f'Running model on {self.device}')
+
     
     if self.model_type == 'ProtoT5':
-      tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_bfd', do_lower_case=False)
-      model = T5Model.from_pretrained("Rostlab/prot_t5_xl_bfd")
+
+      # Loading full percision model requires high ram 24G
+      #tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_bfd', do_lower_case=False)
+      #model = T5Model.from_pretrained("Rostlab/prot_t5_xl_bfd")
+
+      #instead loading half percsion model
+      self.model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(self.device)
+      self.tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
       
-    if self.model_type == 'ProtoBert':
-      tokenizer = #
-      model = #
-      
-    if self.model_type == 'ESM':
-      !pip install fair-esm
-      tokenizer = #
-      model = #
-      
-    self.input_ids = #
-    self.attention_mask,decoder = #
-    self.decoder_input_ids = #
+    
       
       
   def forward(self):
-    embedding = model(input_ids=input_ids,attention_mask=attention_mask,decoder_input_ids=decoder_input_ids)
+
+    token_encoding = self.tokenizer.batch_encode_plus(self.seq, add_special_tokens=True, padding="longest")
+    input_ids      = torch.tensor(token_encoding['input_ids']).to(self.device)
+    attention_mask = torch.tensor(token_encoding['attention_mask']).to(self.device)
+
+    with torch.no_grad():
+
+      embedding = self.model(input_ids=input_ids,attention_mask=attention_mask)
     
     
     return embedding
-      
-      
