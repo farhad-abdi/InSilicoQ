0a5c7d52ea7d7fb4788314c820b08ced402bb381
Farhad Abdi <58492400+farhad-abdi@users.noreply.github.com>
2023-04-13 16:30:14 +0330

    read fasta file

--- insilicoq/utils.py
+++ insilicoq/utils.py
@@ -98,3 +98,33 @@ def graph_to_molecule(graph):
         return None
 
     return molecule
+
+def read_fasta( fasta_path, split_char="!", id_field=0):
+    '''
+        Reads in fasta file containing multiple sequences.
+        Split_char and id_field allow to control identifier extraction from header.
+        E.g.: set split_char="|" and id_field=1 for SwissProt/UniProt Headers.
+        Returns dictionary holding multiple sequences or only single 
+        sequence, depending on input file.
+    '''
+    
+    seqs = dict()
+    with open( fasta_path, 'r' ) as fasta_f:
+        for line in fasta_f:
+            # get uniprot ID from header and create new entry
+            if line.startswith('>'):
+                uniprot_id = line.replace('>', '').strip().split(split_char)[id_field]
+                # replace tokens that are mis-interpreted when loading h5
+                uniprot_id = uniprot_id.replace("/","_").replace(".","_")
+                seqs[ uniprot_id ] = ''
+            else:
+                # repl. all whie-space chars and join seqs spanning multiple lines, drop gaps and cast to upper-case
+                seq= ''.join( line.split() ).upper().replace("-","")
+                # repl. all non-standard AAs and map them to unknown/X
+                seq = seq.replace('U','X').replace('Z','X').replace('O','X')
+                seqs[ uniprot_id ] += seq 
+    example_id=next(iter(seqs))
+    print("Read {} sequences.".format(len(seqs)))
+    print("Example:\n{}\n{}".format(example_id,seqs[example_id]))
+
+    return seqs
